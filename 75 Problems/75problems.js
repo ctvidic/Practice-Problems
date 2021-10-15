756c249ca21f301af9a32ad818413f50d3bc1270
ctvidic <chrisvidic.cv@gmail.com>
2021-10-15 12:27:49 -0400

    leetcode

--- 75 Problems/75problems.js
+++ 75 Problems/75problems.js
@@ -104,7 +104,8 @@ var maxProduct = function (nums) {
 
 
 //#153 Find Minimum in Rotated Sorted Array
-//Summary: Utilize binary search to break up array based of endpoints.
+//Summary: Utilize binary search to break up array based of endpoints, compare to mid-1.
+//Time Complexity: log(N)
 var findMin = function (nums) {
     if (nums.length == 1) {
         return nums[0];
@@ -118,4 +119,87 @@ var findMin = function (nums) {
     } else {
         return findMin(nums.slice(0, mid));
     }
+};
+
+
+//#33. Search in Rotated Sorted Array
+//Summary: Utilize binary search, use logic
+//     IF LEFT < RIGHT --> IF LEFT < TARGET && TARGET < MID
+//     Num must be on left side
+//     IF LEFT < RIGHT --> IF LEFT > TARGET || TARGET > MID
+//     Num must be on right side
+//Use same logic for LEFT > RIGHT case
+var search = function (nums, target) {
+    if (nums == null || nums.length == 0) {
+        return -1;
+    }
+
+    let left = 0
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        if (target == nums[mid]) {
+            return mid;
+        }
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
+
+
+//#15. 3Sum
+// Sort array, for each value less than 0 and not a duplicate,
+// Loop through right half, if sum is greater decrement right, if sum
+// is less than move j left. Solution built off idea that to get 0, one number
+// must be negative (or all 0). Solution avoids duplicates by iteration while loops.
+//Time Complexit: O(N^2)
+var threeSum = function (nums) {
+    var rtn = [];
+    if (nums.length < 3) {
+        return rtn;
+    }
+    nums = nums.sort(function (a, b) {
+        return a - b;
+    });
+    for (var i = 0; i < nums.length - 2; i++) {
+        if (nums[i] > 0) {
+            return rtn;
+        }
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            continue;
+        }
+        for (var j = i + 1, k = nums.length - 1; j < k;) {
+            if (nums[i] + nums[j] + nums[k] === 0) {
+                rtn.push([nums[i], nums[j], nums[k]]);
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1]) {
+                    j++;
+                }
+                while (j < k && nums[k] == nums[k + 1]) {
+                    k--;
+                }
+            } else if (nums[i] + nums[j] + nums[k] > 0) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return rtn;
 };
