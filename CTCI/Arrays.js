5aba75ac08acf5f7b007bd8ccef6faebdd30a2c9
ctvidic <chrisvidic.cv@gmail.com>
2021-10-21 17:04:58 -0400

    arrays

--- CTCI/Arrays.js
+++ CTCI/Arrays.js
@@ -62,3 +62,145 @@ const replaceUrlSpaces = (str) => {
     }
     return convertToArray.join('');
 }
+
+
+//Problem 1.9
+//#48 Rotate Image
+//Summary: First transpose, then reverse values,all while working on one array.
+//Time Complexity: O(N)
+var rotate = function (matrix) {
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = y + 1; x < matrix[y].length; x++) {
+            let temp = matrix[x][y]
+            matrix[x][y] = matrix[y][x];
+            matrix[y][x] = temp;
+        }
+    }
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < Math.floor(matrix.length / 2); x++) {
+            let left = matrix[y][x]
+            let right = matrix[y][matrix.length - x - 1];
+            matrix[y][x] = right;
+            matrix[y][matrix.length - x - 1] = left;
+        }
+    }
+};
+
+
+//Summary: 
+//Problem 1.8
+//#73 Set Matrix Zeros
+//Summary: Identify locations of 0 in first pass, then store row coloumn in hash,
+//second pass remove values;
+
+var setZeroes = function (matrix) {
+    hashX = {};
+    hashY = {};
+    let sorted = false;
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] === 0) {
+                hashX[x] = true;
+                hashY[y] = true;
+            }
+        }
+    }
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (hashX.hasOwnProperty(x)) {
+                matrix[y][x] = 0;
+            }
+            if (hashY.hasOwnProperty(y)) {
+                matrix[y][x] = 0;
+            }
+        }
+    }
+    return matrix;
+};
+
+//In order to do O(1) we must change in place, use a marker
+
+const setZeroes = (matrix) => {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] === 0) {
+                mark(matrix, i, j)
+            }
+        }
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] === null) {
+                matrix[i][j] = 0
+            }
+        }
+    }
+}
+
+const mark = (matrix, row, col) => {
+    for (let j = 0; j < matrix[0].length; j++) {
+        //preserve original 0, mark items that should zero as null
+        if (matrix[row][j] !== 0) {
+            matrix[row][j] = null
+        }
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        //preserve original 0, mark items that should be zero as null
+        if (matrix[i][col] !== 0) {
+            matrix[i][col] = null
+        }
+    }
+}
+
+//Problem 1.9
+//#796 Rotate String
+var rotateString = function (s, goal) {
+    let splitS = s.split('');
+    let splitGoal = goal.split('')
+    for (let i = 0; i < splitS.length; i++) {
+        if (splitS.join('') === splitGoal.join('')) {
+            return true;
+        } else {
+            splitS.unshift(splitS.pop());
+        }
+    }
+    return false;
+};
+//Faster
+var rotateString = function (A, B) {
+    for (let shift = 0; shift < A.length; shift++) if (A.slice(shift) + A.slice(0, shift) === B) return true;
+    return false
+};
+
+//My favorite, reduced space complexity
+var rotateString = function (string1, string2) {
+    if (string1.length !== string2.length) {
+        return false;
+    }
+    return (string2 + string2).includes(string1); 
+};
+
+//Problem 1.6
+//#443 String Compression
+//Summary: Annoying problem, utilize splice and counts to either decrement
+//increment through the array.
+
+var compress = function (chars) {
+    let n = chars.length;
+    let count = 1;
+
+    for (let i = n - 2; i >= 0; i--) {
+        if (chars[i] == chars[i + 1]) {
+            count++;
+        } else if (count > 1) {
+            chars.splice(i + 2, count - 1, ...count.toString().split(''));
+            count = 1;
+        }
+    }
+
+    if (count > 1) {
+        chars.splice(1, count - 1, ...count.toString().split(''));
+    }
+
+    return chars.length;
+};
