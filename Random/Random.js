42a1ecb2cc9a9bc5df3e9189e5715536ea8ff94e
ctvidic <chrisvidic.cv@gmail.com>
2021-11-17 19:16:38 -0500

    stack

--- Random/Random.js
+++ Random/Random.js
@@ -99,6 +99,44 @@ var asteroidCollision = function (asteroids) {
     return asteroids;
 };
 
+//Stack Method
+
+var asteroidCollision = function (asteroids) {
+    const stack = [];
+
+    for (let i = 0; i < asteroids.length; i++) {
+        const curr = asteroids[i];
+
+        if (curr > 0) {
+            stack.push(curr);
+        } else {
+            // if stack not empty
+            // AND if top is positive
+            // AND if top is smaller than curr
+            while (
+                stack.length &&
+                stack[stack.length - 1] > 0 &&
+                stack[stack.length - 1] < Math.abs(curr)
+            ) {
+                stack.pop();
+            }
+
+            // if stack not empty
+            // AND if top has the same size as curr
+            if (stack.length && stack[stack.length - 1] === Math.abs(curr)) {
+                stack.pop();
+            }
+            // if stack is empty
+            // OR top is negative
+            else if (!stack.length || stack[stack.length - 1] < 0) {
+                stack.push(curr);
+            }
+        }
+    }
+
+    return stack;
+};
+
 //1266 Minimum Time Visiting All Points
 //Summary: Diagonal is most efficient, optimize for diagonal
 //Time Complexity: O(N)
